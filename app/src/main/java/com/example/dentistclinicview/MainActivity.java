65125edc90db95dafd5126124dabdce726458b9e
Eric Fan <ericfan2770@gmail.com>
2020-12-15 13:54:22 -0800

    Front/back logic working

--- app/src/main/java/com/example/dentistclinicview/MainActivity.java
+++ app/src/main/java/com/example/dentistclinicview/MainActivity.java
@@ -34,20 +34,33 @@ import android.widget.Toast;
 public class MainActivity extends AppCompatActivity {
     public static final int NUM_TEETH = 32;
 
-    private final int[] toothNum;
     private final int[] teethPh;
+
+    // For front teeth
+    private final int[] toothNum;
     private final int[] teethId;  // the tooth buttons
     private final int[] t_Id;  // the tooth background
     private final int[] phInput;
     private final int[] phId;
+
+    // For back teeth
+    private final int[] back_toothNum;
+    private final int[] back_teethId;  // the tooth buttons
+    private final int[] back_t_Id;  // the tooth background
+    private final int[] back_phInput;
+    private final int[] back_phId;
+
     private boolean showingInput;
     private int currToothId;
 
     private boolean[] teethExists;
+    private boolean[] back_teethExists;
 
     private RadioGroup radioGroup;
     private RadioButton radioButton;
     private Button radioSubmit;
+
+    private boolean isFrontView;
 
     public MainActivity() {
         toothNum = new int[NUM_TEETH];
@@ -57,9 +70,19 @@ public class MainActivity extends AppCompatActivity {
         phId = new int[NUM_TEETH];
         t_Id = new int[NUM_TEETH];
         teethExists = new boolean[NUM_TEETH];
+        back_teethExists = new boolean[NUM_TEETH];
+
+        isFrontView = true;
+
+        back_toothNum = new int[NUM_TEETH];
+        back_teethId = new int[NUM_TEETH];  // the tooth buttons
+        back_t_Id = new int[NUM_TEETH];  // the tooth background
+        back_phInput = new int[NUM_TEETH];
+        back_phId = new int[NUM_TEETH];
 
         for (int i = 0; i < NUM_TEETH; i++) {
             teethExists[i] = true;
+            back_teethExists[i] = true;
         }
 
         // Front tooth logic
@@ -255,6 +278,200 @@ public class MainActivity extends AppCompatActivity {
         phId[31] = R.id.pHTooth32;
         t_Id[31] = R.id.t_32;
 
+        // back teeth
+        // Back tooth logic
+        back_toothNum[0] = R.id.backToothNumber1;
+        back_teethId[0] = R.id.tooth_back_1;
+        back_phInput[0] = R.id.back_pHInputTooth1;
+        back_phId[0] = R.id.back_pHTooth1;
+        back_t_Id[0] = R.id.t_back_1;
+
+        back_toothNum[1] = R.id.backToothNumber2;
+        back_teethId[1] = R.id.tooth_back_2;
+        back_phInput[1] = R.id.back_pHInputTooth2;
+        back_phId[1] = R.id.back_pHTooth2;
+        back_t_Id[1] = R.id.t_back_2;
+
+        back_toothNum[2] = R.id.backToothNumber3;
+        back_teethId[2] = R.id.tooth_back_3;
+        back_phInput[2] = R.id.back_pHInputTooth3;
+        back_phId[2] = R.id.back_pHTooth3;
+        back_t_Id[2] = R.id.t_back_3;
+
+        back_toothNum[3] = R.id.backToothNumber4;
+        back_teethId[3] = R.id.tooth_back_4;
+        back_phInput[3] = R.id.back_pHInputTooth4;
+        back_phId[3] = R.id.back_pHTooth4;
+        back_t_Id[3] = R.id.t_back_4;
+
+        back_toothNum[4] = R.id.backToothNumber5;
+        back_teethId[4] = R.id.tooth_back_5;
+        back_phInput[4] = R.id.back_pHInputTooth5;
+        back_phId[4] = R.id.back_pHTooth5;
+        back_t_Id[4] = R.id.t_back_5;
+
+        back_toothNum[5] = R.id.backToothNumber6;
+        back_teethId[5] = R.id.tooth_back_6;
+        back_phInput[5] = R.id.back_pHInputTooth6;
+        back_phId[5] = R.id.back_pHTooth6;
+        back_t_Id[5] = R.id.t_back_6;
+
+        back_toothNum[6] = R.id.backToothNumber7;
+        back_teethId[6] = R.id.tooth_back_7;
+        back_phInput[6] = R.id.back_pHInputTooth7;
+        back_phId[6] = R.id.back_pHTooth7;
+        back_t_Id[6] = R.id.t_back_7;
+
+        back_toothNum[7] = R.id.backToothNumber8;
+        back_teethId[7] = R.id.tooth_back_8;
+        back_phInput[7] = R.id.back_pHInputTooth8;
+        back_phId[7] = R.id.back_pHTooth8;
+        back_t_Id[7] = R.id.t_back_8;
+
+        back_toothNum[8] = R.id.backToothNumber9;
+        back_teethId[8] = R.id.tooth_back_9;
+        back_phInput[8] = R.id.back_pHInputTooth9;
+        back_phId[8] = R.id.back_pHTooth9;
+        back_t_Id[8] = R.id.t_back_9;
+
+        back_toothNum[9] = R.id.backToothNumber10;
+        back_teethId[9] = R.id.tooth_back_10;
+        back_phInput[9] = R.id.back_pHInputTooth10;
+        back_phId[9] = R.id.back_pHTooth10;
+        back_t_Id[9] = R.id.t_back_10;
+
+        back_toothNum[10] = R.id.backToothNumber11;
+        back_teethId[10] = R.id.tooth_back_11;
+        back_phInput[10] = R.id.back_pHInputTooth11;
+        back_phId[10] = R.id.back_pHTooth11;
+        back_t_Id[10] = R.id.t_back_11;
+
+        back_toothNum[11] = R.id.backToothNumber12;
+        back_teethId[11] = R.id.tooth_back_12;
+        back_phInput[11] = R.id.back_pHInputTooth12;
+        back_phId[11] = R.id.back_pHTooth12;
+        back_t_Id[11] = R.id.t_back_12;
+
+        back_toothNum[12] = R.id.backToothNumber13;
+        back_teethId[12] = R.id.tooth_back_13;
+        back_phInput[12] = R.id.back_pHInputTooth13;
+        back_phId[12] = R.id.back_pHTooth13;
+        back_t_Id[12] = R.id.t_back_13;
+
+        back_toothNum[13] = R.id.backToothNumber14;
+        back_teethId[13] = R.id.tooth_back_14;
+        back_phInput[13] = R.id.back_pHInputTooth14;
+        back_phId[13] = R.id.back_pHTooth14;
+        back_t_Id[13] = R.id.t_back_14;
+
+        back_toothNum[14] = R.id.backToothNumber15;
+        back_teethId[14] = R.id.tooth_back_15;
+        back_phInput[14] = R.id.back_pHInputTooth15;
+        back_phId[14] = R.id.back_pHTooth15;
+        back_t_Id[14] = R.id.t_back_15;
+
+        back_toothNum[15] = R.id.backToothNumber16;
+        back_teethId[15] = R.id.tooth_back_16;
+        back_phInput[15] = R.id.back_pHInputTooth16;
+        back_phId[15] = R.id.back_pHTooth16;
+        back_t_Id[15] = R.id.t_back_16;
+
+        back_toothNum[16] = R.id.backToothNumber17;
+        back_teethId[16] = R.id.tooth_back_17;
+        back_phInput[16] = R.id.back_pHInputTooth17;
+        back_phId[16] = R.id.back_pHTooth17;
+        back_t_Id[16] = R.id.t_back_17;
+
+        back_toothNum[17] = R.id.backToothNumber18;
+        back_teethId[17] = R.id.tooth_back_18;
+        back_phInput[17] = R.id.back_pHInputTooth18;
+        back_phId[17] = R.id.back_pHTooth18;
+        back_t_Id[17] = R.id.t_back_18;
+
+        back_toothNum[18] = R.id.backToothNumber19;
+        back_teethId[18] = R.id.tooth_back_19;
+        back_phInput[18] = R.id.back_pHInputTooth19;
+        back_phId[18] = R.id.back_pHTooth19;
+        back_t_Id[18] = R.id.t_back_19;
+
+        back_toothNum[19] = R.id.backToothNumber20;
+        back_teethId[19] = R.id.tooth_back_20;
+        back_phInput[19] = R.id.back_pHInputTooth20;
+        back_phId[19] = R.id.back_pHTooth20;
+        back_t_Id[19] = R.id.t_back_20;
+
+        back_toothNum[20] = R.id.backToothNumber21;
+        back_teethId[20] = R.id.tooth_back_21;
+        back_phInput[20] = R.id.back_pHInputTooth21;
+        back_phId[20] = R.id.back_pHTooth21;
+        back_t_Id[20] = R.id.t_back_21;
+
+        back_toothNum[21] = R.id.backToothNumber22;
+        back_teethId[21] = R.id.tooth_back_22;
+        back_phInput[21] = R.id.back_pHInputTooth22;
+        back_phId[21] = R.id.back_pHTooth22;
+        back_t_Id[21] = R.id.t_back_22;
+
+        back_toothNum[22] = R.id.backToothNumber23;
+        back_teethId[22] = R.id.tooth_back_23;
+        back_phInput[22] = R.id.back_pHInputTooth23;
+        back_phId[22] = R.id.back_pHTooth23;
+        back_t_Id[22] = R.id.t_back_23;
+
+        back_toothNum[23] = R.id.backToothNumber24;
+        back_teethId[23] = R.id.tooth_back_24;
+        back_phInput[23] = R.id.back_pHInputTooth24;
+        back_phId[23] = R.id.back_pHTooth24;
+        back_t_Id[23] = R.id.t_back_24;
+
+        back_toothNum[24] = R.id.backToothNumber25;
+        back_teethId[24] = R.id.tooth_back_25;
+        back_phInput[24] = R.id.back_pHInputTooth25;
+        back_phId[24] = R.id.back_pHTooth25;
+        back_t_Id[24] = R.id.t_back_25;
+
+        back_toothNum[25] = R.id.backToothNumber26;
+        back_teethId[25] = R.id.tooth_back_26;
+        back_phInput[25] = R.id.back_pHInputTooth26;
+        back_phId[25] = R.id.back_pHTooth26;
+        back_t_Id[25] = R.id.t_back_26;
+
+        back_toothNum[26] = R.id.backToothNumber27;
+        back_teethId[26] = R.id.tooth_back_27;
+        back_phInput[26] = R.id.back_pHInputTooth27;
+        back_phId[26] = R.id.back_pHTooth27;
+        back_t_Id[26] = R.id.t_back_27;
+
+        back_toothNum[27] = R.id.backToothNumber28;
+        back_teethId[27] = R.id.tooth_back_28;
+        back_phInput[27] = R.id.back_pHInputTooth28;
+        back_phId[27] = R.id.back_pHTooth28;
+        back_t_Id[27] = R.id.t_back_28;
+
+        back_toothNum[28] = R.id.backToothNumber29;
+        back_teethId[28] = R.id.tooth_back_29;
+        back_phInput[28] = R.id.back_pHInputTooth29;
+        back_phId[28] = R.id.back_pHTooth29;
+        back_t_Id[28] = R.id.t_back_29;
+
+        back_toothNum[29] = R.id.backToothNumber30;
+        back_teethId[29] = R.id.tooth_back_30;
+        back_phInput[29] = R.id.back_pHInputTooth30;
+        back_phId[29] = R.id.back_pHTooth30;
+        back_t_Id[29] = R.id.t_back_30;
+
+        back_toothNum[30] = R.id.backToothNumber31;
+        back_teethId[30] = R.id.tooth_back_31;
+        back_phInput[30] = R.id.back_pHInputTooth31;
+        back_phId[30] = R.id.back_pHTooth31;
+        back_t_Id[30] = R.id.t_back_31;
+
+        back_toothNum[31] = R.id.backToothNumber32;
+        back_teethId[31] = R.id.tooth_back_32;
+        back_phInput[31] = R.id.back_pHInputTooth32;
+        back_phId[31] = R.id.back_pHTooth32;
+        back_t_Id[31] = R.id.t_back_32;
+
         showingInput = false;
         currToothId = -1;
     }
@@ -267,6 +484,7 @@ public class MainActivity extends AppCompatActivity {
 
         for (int i = 0; i < NUM_TEETH; i++) {
             handleTooth(i, toothNum[i], teethId[i], phInput[i], phId[i], t_Id[i]);
+            handleTooth(i, back_toothNum[i], back_teethId[i], back_phInput[i], back_phId[i], back_t_Id[i]);
         }
 
         TextView frontBackTextView = findViewById(R.id.frontBackTextView);
@@ -280,25 +498,50 @@ public class MainActivity extends AppCompatActivity {
                 if (radioButton.equals(findViewById(R.id.radioFront))) {
                     // The front radio button is selected
                     frontBackTextView.setText("Front View");
+                    isFrontView = true;
 
+                    // make all the front tooth logic show up
                     for (int i = 0; i < NUM_TEETH; i++) {
                         if (teethExists[i]) {
+                            // the back layer tooth
                             ImageButton button = findViewById(teethId[i]);
                             button.setVisibility(View.VISIBLE);
 
+                            // the front layer tooth
                             ImageView tooth = findViewById(t_Id[i]);
                             tooth.setVisibility(View.VISIBLE);
 
+                            // the actual ph display
                             TextView phDisplay = findViewById(phId[i]);
                             phDisplay.setVisibility(View.VISIBLE);
                         }
+                        // make the tooth number show up
+                        TextView toothNumber = findViewById(toothNum[i]);
+                        toothNumber.setVisibility(View.VISIBLE);
                     }
 
+                    // Make all of the front teeth tools disappear
+                    for (int i = 0; i < NUM_TEETH; i++) {
+                        ImageButton button = findViewById(back_teethId[i]);
+                        button.setVisibility(View.INVISIBLE);
+
+                        ImageView tooth = findViewById(back_t_Id[i]);
+                        tooth.setVisibility(View.INVISIBLE);
+
+                        TextView phDisplay = findViewById(back_phId[i]);
+                        phDisplay.setVisibility(View.INVISIBLE);
+
+                        // make the tooth number show up
+                        TextView toothNumber = findViewById(back_toothNum[i]);
+                        toothNumber.setVisibility(View.INVISIBLE);
+                    }
                 } else {
+                    isFrontView = false;
+
                     // The back radio button is selected
                     frontBackTextView.setText("Back View");
 
-                    // Make all of the front teeth disappear
+                    // Make all of the front teeth tools disappear
                     for (int i = 0; i < NUM_TEETH; i++) {
                         ImageButton button = findViewById(teethId[i]);
                         button.setVisibility(View.INVISIBLE);
@@ -308,7 +551,31 @@ public class MainActivity extends AppCompatActivity {
 
                         TextView phDisplay = findViewById(phId[i]);
                         phDisplay.setVisibility(View.INVISIBLE);
+
+                        // make the tooth number show up
+                        TextView toothNumber = findViewById(toothNum[i]);
+                        toothNumber.setVisibility(View.INVISIBLE);
                     }
+
+                    // Make the back teeth logic and tools appear
+                    for (int i = 0; i < NUM_TEETH; i++) {
+                        if (back_teethExists[i]) {
+                            ImageButton button = findViewById(back_teethId[i]);
+                            button.setVisibility(View.VISIBLE);
+
+                            ImageView tooth = findViewById(back_t_Id[i]);
+                            tooth.setVisibility(View.VISIBLE);
+
+                            // the actual ph display
+                            TextView phDisplay = findViewById(back_phId[i]);
+                            phDisplay.setVisibility(View.VISIBLE);
+                        }
+
+                        // make the tooth number show up
+                        TextView toothNumber = findViewById(back_toothNum[i]);
+                        toothNumber.setVisibility(View.VISIBLE);
+                    }
+
                 }
             }
         });
@@ -382,7 +649,12 @@ public class MainActivity extends AppCompatActivity {
                                 button.setVisibility(View.INVISIBLE);
                                 phDisplay.setVisibility(View.INVISIBLE);
                                 phInput.setVisibility(View.INVISIBLE);
-                                teethExists[tooth] = false;
+                                if (isFrontView) {
+                                    teethExists[tooth] = false;
+                                } else {
+                                    back_teethExists[tooth] = false;
+                                }
+
                             }
                         });
 
@@ -449,7 +721,12 @@ public class MainActivity extends AppCompatActivity {
                 button.setVisibility(View.VISIBLE);
                 phDisplay.setVisibility(View.VISIBLE);
                 frontView.setVisibility(View.VISIBLE);
-                teethExists[tooth] = true;
+                if (isFrontView) {
+                    teethExists[tooth] = true;
+                } else {
+                    back_teethExists[tooth] = true;
+                }
+
             }
         });
     }
